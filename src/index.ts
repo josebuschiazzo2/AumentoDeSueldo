9184b37f1e4a178990e489558b8d5f19d0f9bdfc
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-21 20:24:49 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import "./styles.css";
+/*Aumento de sueldo*/
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+/* defino las constantes de aumento de sueldo*/
+const increase1 = 0.20;
+const increase2 = 0.10;
+const increase3 = 0.05;
+
+
+let salary:number= Number(prompt("Ingrese su salario (sin signo de moneda ni coma)"));
+
+// pregunto primero si introdujo un número
+if (isNaN(salary))
+  {
+  alert ("Solo son validos los números");
+  } else {
+      if (salary<15001)
+        { console.log("Su salario se incrementa un 20%, quedando el mismo en " + (salary*(1+increase1)).toFixed(2)); // recorto a 2 unidades despues de la coma 
+        } else {
+            if (salary<20001)
+              {console.log("Su salario se incrementa un 10%, quedando el mismo en " + (salary*(1+increase2)).toFixed(2)); // recorto a 2 unidades despues de la coma
+              } else {
+                  if (salary<25001)
+                  {console.log("Su salario se incrementa un 5%, quedando el mismo en " + (salary*(1+increase3)).toFixed(2)); // recorto a 2 unidades despues de la coma
+                  } else {
+                    console.log("Su salario momentaneamente no tiene incremento");
+                      } 
+                }
+            }
+
+        }
